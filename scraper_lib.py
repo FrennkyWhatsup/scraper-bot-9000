ababd0ece8d52ec53615b877154000960152cf2a
Martin Nefjodov <Martin.Nefjodov@agileautomations.co.uk>
2021-06-17 22:38:20 +0100

    fix runtime error, add more comments

--- scraper_lib.py
+++ scraper_lib.py
@@ -12,11 +12,18 @@ class PageReader(str):
     def __init__(self, site: str):
         self._site = site
 
+    # this is called when we do "PageReader(some_url).open()"
+    # or "with PageReader(some_url) as page_reader"
     def __enter__(self):
         self.driver = webdriver.Chrome()
         self.driver.get(self._site)
         return self
 
+    # this is called when we do "PageReader(some_url).close()"
+    # or when we exit / return from this code block
+    #   with PageReader(some_url) as page_reader:
+    #       ...
+    #       return
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.driver.close()
 
@@ -30,7 +37,7 @@ class PageReader(str):
         return self.driver.find_element_by_tag_name('body')
 
     def get_element(self, xpath: str) -> WebElement:
-        return self.get_page().find_elements_by_xpath(xpath)
+        return self.get_page().find_element_by_xpath(xpath)
 
     def get_elements(self, xpath: str) -> List[WebElement]:
         return self.get_page().find_elements_by_xpath(xpath)
